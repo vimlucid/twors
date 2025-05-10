20ca52e186a3d63bc130da02d8ad8942569b0291
Lyudmil Grigorov <lyudmil.grigorov@lucidlink.com>
2025-05-10 20:16:25 +0300

    #1 Rename macro crate

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,5 +2,5 @@ pub use crate::{
     ComponentGetter, ComponentLifecycle, Context, Engine, Key, Layer, Mouse, Renderable, Transform,
     Vertex2,
 };
-pub use macros::Component;
+pub use twors_derive::Component;
 pub use web_sys::CanvasRenderingContext2d;
